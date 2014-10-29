d939c28e48b10a98a7dbf7ff89e41c3295abb8e0
Marc Garreau <marcdgarreau@gmail.com>
2014-10-28 19:23:48 -0600

    Adds post properties test

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -8,4 +8,10 @@ describe Post, type: :model do
     expect(post).to be_valid
   end
 
+  it "is valid with properties field" do
+    post.properties = { twitter_url: "twitter.com", tweet_date: "10/27/2014" }
+    expect(post.properties["twitter_url"]).to eq("twitter.com")
+    expect(post.properties["tweet_date"]).to eq("10/27/2014")
+    expect(post).to be_valid
+  end
 end
