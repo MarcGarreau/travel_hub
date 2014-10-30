bee8a4ac1ec18c497b10a296d916ce6179e1142e
timproctor <timandcamiproctor@gmail.com>
2014-10-30 17:15:32 -0600

    created api controller, trips, and posts controller, created api routes and nested routes for trips and posts" "

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#bounce', as: :login
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match '/auth/failure', to: 'sessions#failure', via: [:get, :post]
+
+  namespace :api do
+    namespace :v1 do
+      resources :trips, only: [:index, :show] do
+        resources :posts, only: [:index, :show]
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029201700) do
+ActiveRecord::Schema.define(version: 20141030155931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20141029201700) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.hstore   "properties"
+  end
+
+  create_table "trips", force: true do |t|
+    t.string   "title"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
