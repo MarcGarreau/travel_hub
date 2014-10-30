e63ad102582d308bdd51e7217a99a76e9898f50a
Emily Davis <emilyadavis303@gmail.com>
2014-10-30 11:42:24 -0600

    Tidies up trip_spec and test user/test relationship in user_spec

--- spec/model/trip_spec.rb
+++ spec/model/trip_spec.rb
@@ -1,76 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  it "is valid" do
-    trip = Trip.create( title:      "Vegas Baby!",
-                        start_date: "2014-10-30",
-                        end_date:   "2014-10-31",
-                        user_id:    1)
+  let(:trip) { Trip.create( title:      "Vegas Baby!, Baby?, Uh oh.",
+                            start_date: "2014-10-30",
+                            end_date:   "2014-11-10",
+                            user_id:    1 ) }
 
+  it "is valid" do
     expect(trip).to be_valid
   end
 
   it "is invalid without title" do
-    trip = Trip.create( title:      nil,
-                        start_date: "2014-10-30",
-                        end_date:   "2014-10-31",
-                        user_id:    1)
+    trip.title = nil
 
     expect(trip).not_to be_valid
   end
 
   it "is invalid without start_date" do
-    trip = Trip.create( title:      "Vegas Baby!, Baby?, I'm prego",
-                        start_date: nil,
-                        end_date:   "2014-10-31",
-                        user_id:    1)
+    trip.start_date = nil
 
     expect(trip).not_to be_valid
   end
 
   it "is invalid without end_date" do
-    trip = Trip.create( title:      "Vegas Baby!",
-                        start_date: "2014-10-30",
-                        end_date:   nil,
-                        user_id:    1)
+    trip.end_date = nil
 
     expect(trip).not_to be_valid
   end
 
   it "is invalid without a user_id" do
-    trip = Trip.create( title:      "Maui Wowi",
-                        start_date: "2014-10-30",
-                        end_date:   "2014-11-02",
-                        user_id:    nil)
+    trip.user_id = nil
 
     expect(trip).not_to be_valid
   end
 
   it "shows formatted start_date" do
-    trip = Trip.create( title:      "Oh Canada!",
-                        start_date: "2014-10-30",
-                        end_date:   "2014-11-03",
-                        user_id:    1)
-
     expect(trip.format_start_date).to eq("Oct 30, 2014")
   end
 
   it "shows formatted end_date" do
-    trip = Trip.create( title:      "Oh Canada!",
-                        start_date: "2014-10-30",
-                        end_date:   "2014-11-03",
-                        user_id:    1)
-
-    expect(trip.format_end_date).to eq("Nov 03, 2014")
+    expect(trip.format_end_date).to eq("Nov 10, 2014")
   end
 
   it "knows its user" do
-    user = User.create( name: "Boogly-Boo",
-                        nickname: "boo-boo")
-    trip = Trip.create( title: "Oh Canada!",
-                        start_date: "2014-10-30",
-                        end_date: "2014-11-03",
-                        user_id: user.id)
+    user = User.create( name: "Boogly-Boo" )
+    trip.user_id = user.id
 
     expect(trip.user.name).to eq("Boogly-Boo")
   end
