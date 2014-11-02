c14f317b476bc31e3685ba29b3ef4a223e0a5340
Emily Davis <emilyadavis303@gmail.com>
2014-11-02 16:53:04 -0700

    step one.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,26 @@
 class TripsController < ApplicationController
   def new
-    
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create(trip_params)
+
+    if @trip.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title,
+                                 :start_date,
+                                 :end_date,
+                                 :user_id)
   end
 end
---
+++ spec/controllers/trips_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe TripsController, type: :controller do
+  include Builders
+
+  describe "GET new" do
+    it "assigns a new trip @trip" do
+      get :new
+      expect(assigns(:trip)).to be_a_new(Trip)
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe "POST create" do
+    describe "with valid params" do
+      it "saves the new trip in the database" do
+        trip_attributes
+
+        expect { post :create, trip: @attributes}.to change(Trip, :count).by 1
+      end
+
+      it "redirects to the trip's show page" do
+        trip_attributes
+        post :create, trip: @attributes
+
+        expect(response).to redirect_to(Trip.last)
+      end
+    end
+
+    describe "with invalid params" do
+      xit "does not save the new contact in the database" do
+      end
+
+      xit "re-renders the :new template" do
+      end
+    end
+  end
+end
