5db077e9bdda3398bd847272453a7b46eeca9bd6
Horacio Chavez <horacio.hch@gmail.com>
2014-11-05 13:48:42 -0700

    added hide field to posts, added travels join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104164640) do
+ActiveRecord::Schema.define(version: 20141105204736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141104164640) do
     t.datetime "updated_at"
     t.hstore   "properties"
     t.string   "source"
+    t.boolean  "hide"
   end
 
   create_table "travels", id: false, force: true do |t|
