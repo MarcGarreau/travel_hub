6f3bffc65f843cfdb662759cd25e3892adae0bbd
Horacio Chavez <horacio.hch@gmail.com>
2014-11-12 17:16:31 -0700

    wip

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,9 @@
 module PostsHelper
   def wordy_time(post)
-    from_time = post.created_date.to_time
-    to_time   = Time.now
-    distance_of_time_in_words(from_time, to_time, options = {include_seconds: true})
+    unless post.created_date.to_time != "" || from_time != nil
+      from_time = post.created_date.to_time
+      to_time   = Time.now
+      distance_of_time_in_words(from_time, to_time, options = {include_seconds: true}) + " ago"
+    end
   end
 end
