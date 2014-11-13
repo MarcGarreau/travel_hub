d403293922843023dadfce6c0f5f9bb798bbb9f4
Horacio Chavez <horacio.hch@gmail.com>
2014-11-12 20:35:21 -0700

    Time wordy works

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,7 @@
 module PostsHelper
   def wordy_time(post)
-    unless post.created_date.to_time != "" || from_time != nil
-      from_time = post.created_date.to_time
-      to_time   = Time.now
-      distance_of_time_in_words(from_time, to_time, options = {include_seconds: true}) + " ago"
-    end
+    from_time = post.created_date.to_time
+    to_time   = Time.now
+    distance_of_time_in_words(from_time, to_time, options = {include_seconds: true}) + " ago"
   end
 end
